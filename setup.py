f5550245232c49e24f28f86ec9e976a7c12847c9
Rakesh Medhari <medharirakeshavs@gmail.com>
2026-03-06 15:10:24 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Rakesh',
+    author_email='medharirakeshavs@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
